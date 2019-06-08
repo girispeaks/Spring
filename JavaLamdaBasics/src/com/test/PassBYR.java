532bb9c29e7e20205137da470cf78090b3aa361a
Girish R <girispeaks@gmail.com>
2019-06-08 13:49:54 +0530

    adding movie catalog project

---
+++ JavaLamdaBasics/src/com/test/PassBYR.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class PassBYR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str=new String("test");
+		System.out.println(chngStr(str));
+		System.out.println(str);
+	}
+	
+	public static String chngStr(String str) {
+		return str="testing";
+	}
+
+}
